2200fa4d30c4fd445c9c1495e96863063d8b2566
janisbe <janis666@gmail.com>
2021-02-08 00:01:45 +0100

    zmiana w POM + konfiguracja

--- backend/src/main/java/com/janis/pcmanager/config/WebConfig.java
+++ backend/src/main/java/com/janis/pcmanager/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.janis.pcmanager.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
