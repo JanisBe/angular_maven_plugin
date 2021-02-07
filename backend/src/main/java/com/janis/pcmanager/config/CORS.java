81c24e29de2debd4f25ee1d86544aae8f646daff
janisbe <janis666@gmail.com>
2021-02-07 21:08:57 +0100

    dodany CORS i WebConfig

--- backend/src/main/java/com/janis/pcmanager/config/CORS.java
+++ backend/src/main/java/com/janis/pcmanager/config/CORS.java
@@ -1,17 +1,15 @@
-package com.janis.pcmanager;
+package com.janis.pcmanager.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
+public class CORS extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
                 .allowedHeaders("*");
+        System.out.println("CORS");
     }
 }
